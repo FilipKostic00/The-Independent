b7620dfd794ff9ba59fe7a53529a7a846ae03db5
FilipKostic00 <fkostic00@gmail.com>
2024-04-11 13:28:18 +0200

    hdr, bloom, readme

---
+++ resources/shaders/hdr.fs
@@ -0,0 +1,32 @@
+#version 460 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D scene;
+uniform sampler2D bloomBlur;
+uniform bool hdr;
+uniform bool bloom;
+uniform float exposure;
+uniform float gamma;
+
+void main()
+{
+    vec3 hdrColor = texture(scene, TexCoords).rgb;
+    vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
+    if(hdr)
+    {
+        if(bloom)
+            hdrColor += bloomColor;
+        // exposure
+        vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
+        // also gamma correct while we're at it
+        result = pow(result, vec3(1.0 / gamma));
+        FragColor = vec4(result, 1.0);
+    }
+    else
+    {
+        vec3 result = pow(hdrColor, vec3(1.0 / gamma));
+        FragColor = vec4(result, 1.0);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 
 #include <iostream>
 
+void renderQuad();
+
 unsigned int loadCubemap(vector<std::string> faces);
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -97,8 +99,12 @@ struct ProgramState {
     float roundTableModelScale = 1.0f;
     float candleModelScale = 0.1f;
     float firewoodModelScale = 0.1f;
+    float exposure = 0.6f;
+    float gamma = 1.255f;
     bool blinn = false;
     bool isCamSpotLightEnabled = false;
+    bool hdr = true;
+    bool bloom = true;
     PointLight eyePointLight1;
     PointLight eyePointLight2;
     PointLight candlePointLight;
@@ -267,6 +273,61 @@ int main() {
     // -------------------------
     Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
     Shader skyboxShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
+    Shader hdrShader("resources/shaders/hdr.vs", "resources/shaders/hdr.fs");
+    Shader blurShader("resources/shaders/blur.vs", "resources/shaders/blur.fs");
+
+    unsigned int hdrFBO;
+    glGenFramebuffers(1, &hdrFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+    // create 2 floating point color buffers (1 for normal rendering, other for brightness threshold values)
+    unsigned int colorBuffers[2];
+    glGenTextures(2, colorBuffers);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        // attach texture to framebuffer
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+    }
+    // create and attach depth buffer (renderbuffer)
+    unsigned int rboDepth;
+    glGenRenderbuffers(1, &rboDepth);
+    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+    // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering
+    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+    glDrawBuffers(2, attachments);
+    // finally check if framebuffer is complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // ping-pong-framebuffer for blurring
+    unsigned int pingpongFBO[2];
+    unsigned int pingpongColorbuffers[2];
+    glGenFramebuffers(2, pingpongFBO);
+    glGenTextures(2, pingpongColorbuffers);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
+        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
+        // also check if framebuffers are complete (no need for depth buffer)
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "Framebuffer not complete!" << std::endl;
+    }
+
+
 
     // load models
     // -----------
@@ -321,17 +382,6 @@ int main() {
     eyePointLight2.constant = 0.505f;
     eyePointLight2.linear = 0.025f;
     eyePointLight2.quadratic = 0.015f;
-
-    //Candle point light
-    PointLight& candlePointLight = programState->candlePointLight;
-    candlePointLight.position = glm::vec3(0.90f, -1.7f, -1.545f);
-    candlePointLight.ambient = glm::vec3(0.2f, 0.2f, 0.2f);
-    candlePointLight.diffuse = glm::vec3(0.5f, 0.5f, 0.5f);
-    candlePointLight.specular = glm::vec3(1.0, 1.0, 1.0);
-
-    candlePointLight.constant = 1.0f;
-    candlePointLight.linear = 0.09f;
-    candlePointLight.quadratic = 0.032f;
 
     //Camera Spotlight
     SpotLight& cameraSpotlight = programState->cameraSpotLight;
@@ -424,6 +474,11 @@ int main() {
             };
     unsigned int cubemapTexture = loadCubemap(faces);
 
+    hdrShader.use();
+    hdrShader.setInt("scene", 0);
+    hdrShader.setInt("blumBloor",0);
+    blurShader.use();
+    blurShader.setInt("image", 0);
 
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -447,8 +502,17 @@ int main() {
         glClearColor(programState->clearColor.r, programState->clearColor.g, programState->clearColor.b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // don't forget to enable shader before setting uniforms
+        glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         ourShader.use();
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(programState->camera.Zoom),
+                                                (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = programState->camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
         ourShader.setVec3("viewPosition", programState->camera.Position);
         //Dir Light
         ourShader.setVec3("dirLight.direction", dirLight.direction);
@@ -491,38 +555,8 @@ int main() {
         ourShader.setFloat("cameraSpotLight.cutOff", cameraSpotlight.cutOff);
         ourShader.setFloat("cameraSpotLight.outerCutOff", cameraSpotlight.outerCutOff);
 
-
-
-
-        // Candle point light
-        // TO DO: Place this in imgui and fix light position
-//        glm::vec3 candleFlameColor = glm::vec3(0.1f, 0.05f, 0.01f); // Example flame color (adjust as needed)
-//        float pulseSpeed = 0.5f; // Adjust the speed of pulsation as needed
-//
-//        // Time-based modulation for pulsating effect
-//        float intensityModifier = 2.0f + 0.5f * sin(currentFrame * pulseSpeed);
-//
-//        candlePointLight.ambient = candleFlameColor;
-//        candlePointLight.diffuse = candleFlameColor;
-//        candlePointLight.specular = candleFlameColor;
-//
-//        ourShader.setVec3("candlePointLight.ambient", candlePointLight.ambient);
-//        ourShader.setVec3("candlePointLight.diffuse", candlePointLight.diffuse);
-//        ourShader.setVec3("candlePointLight.specular", candlePointLight.specular);
-//        ourShader.setFloat("candlePointLight.constant", candlePointLight.constant);
-//        ourShader.setFloat("candlePointLight.linear", candlePointLight.linear);
-//        ourShader.setFloat("candlePointLight.quadratic", candlePointLight.quadratic);
-
-
         ourShader.setBool("blinn", programState->blinn);
         ourShader.setFloat("isCamSpotLightEnabled", programState->isCamSpotLightEnabled);
-
-        // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(programState->camera.Zoom),
-                                                (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = programState->camera.GetViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
 
         // render the island model
         glm::mat4 model = glm::mat4(1.0f);
@@ -629,6 +663,35 @@ int main() {
         glBindVertexArray(0);
         glDepthFunc(GL_LESS); // set depth function back to default
 
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        bool horizontal = true, first_iteration = true;
+        unsigned int amount = 10;
+        blurShader.use();
+        for (unsigned int i = 0; i < amount; i++)
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
+            blurShader.setInt("horizontal", horizontal);
+            glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorbuffers[!horizontal]);  // bind texture of other framebuffer (or scene if first iteration)
+            renderQuad();
+            horizontal = !horizontal;
+            if (first_iteration)
+                first_iteration = false;
+        }
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        hdrShader.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[!horizontal]);
+        hdrShader.setInt("hdr", programState->hdr);
+        hdrShader.setInt("bloom", programState->bloom);
+        hdrShader.setFloat("exposure", programState->exposure);
+        hdrShader.setFloat("gamma", programState->gamma);
+        renderQuad();
+
         if (programState->ImGuiEnabled)
             DrawImGui(programState);
 
@@ -648,6 +711,36 @@ int main() {
     glfwTerminate();
     return 0;
 }
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+                // positions        // texture Coords
+                -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+                -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+                1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
+
 // Load skybox texture
 unsigned int loadCubemap(vector<std::string> faces)
 {
@@ -738,7 +831,20 @@ void DrawImGui(ProgramState *programState) {
     {
         static float f = 0.0f;
         ImGui::Begin("Controls");
-        ImGui::Text("Controls");
+        ImGui::Text(programState->hdr ? "HDR ON" : "HDR OFF");
+        ImGui::Text(programState->bloom ? "Bloom ON" : "Bloom OFF");
+        if(ImGui::CollapsingHeader("HDR"))
+        {
+            ImGui::DragFloat("Exposure", (float *) &programState->exposure, 0.005f, 0.1f, 1.0f);
+            ImGui::DragFloat("Gamma", (float *) &programState->gamma, 0.005f, 0.1f, 4.0f);
+        }
+
+        if(ImGui::CollapsingHeader("Bloom"))
+        {
+            ImGui::DragFloat("Exposure", (float *) &programState->exposure, 0.005f, 0.1f, 1.0f);
+            ImGui::DragFloat("Gamma", (float *) &programState->gamma, 0.005f, 0.1f, 4.0f);
+        }
+
         if(ImGui::CollapsingHeader("Backgorund"))
         {
             if(ImGui::TreeNode("Color"))
@@ -1004,4 +1110,8 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
         programState->blinn = !programState->blinn;
     if(key == GLFW_KEY_F && action == GLFW_PRESS)
         programState->isCamSpotLightEnabled = !programState->isCamSpotLightEnabled;
+    if(key == GLFW_KEY_H && action == GLFW_PRESS)
+        programState->hdr = !programState->hdr;
+    if(key == GLFW_KEY_J && action == GLFW_PRESS)
+        programState->bloom = !programState->bloom;
 }
